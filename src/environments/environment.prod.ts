5114ace3fc4339bad092da065538fc0cec469ef1
Ryuytyuu-Greseven <rukku93223@gmail.com>
2022-08-15 17:13:27 +0530

    misc fixes.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.1.22:9323/',
+  apiUrl: 'http://13.234.19.246:9323/',
   enc_key:
     '#^&*NA#T)%!UR&E&*RY&$UYT/;YU^&U$@#NEXUS(SOCIAL$%KEY&)mindplay#%*^&@$89SPACE@#$93223%^&*(^',
 };
